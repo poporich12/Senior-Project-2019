4b328ac17312f3e68ec34b7babce1193f24d00ed
Cayanat_P <nanpeaw44@gmail.com>
2019-08-28 16:29:39 +0700

    ...

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ imgs=[cv2.imread('00.jpg',0),cv2.imread('01.jpg',0),cv2.imread('02.jpg',0),cv2.i
 
 for i in range(7):
     #img = cv2.imread('01.jpg',0)
-    #_, img = cv2.threshold(imgs[i],50,255,cv2.THRESH_BINARY)
-    #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    _, img = cv2.threshold(imgs[i],50,255,cv2.THRESH_BINARY)
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     img = cv2.resize(imgs[i],TARGET_SIZE)
     equ = cv2.equalizeHist(img)
     new_img = cv2.convertScaleAbs(img,2,2)
